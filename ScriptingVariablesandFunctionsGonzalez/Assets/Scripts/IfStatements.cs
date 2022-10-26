c5091dffa2682bb2567241cab03183d8f654139b
zoeslemonicedtea <720643@student.dusd.net>
2022-10-26 09:05:01 -0700

    Added Our Project
    Added our scripting project

---
+++ ScriptingVariablesandFunctionsGonzalez/Assets/Scripts/IfStatements.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    float coffeeTemperature = 85.0f;
+    float hotLimitTemperature = 70.0f;
+    float coldLimitTemperature = 40.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            TemperatureTest();
+
+        coffeeTemperature -= Time.deltaTime * 5f;
+        
+    }
+
+
+    void TemperatureTest ()
+    { 
+        //If the coffee's temperature is greater than the hottest drinking 
+}
